9d0055d1e6b6a13717fcd7655013c404d7c3d060
Robert Smith <rsmith1412@yahoo.com>
2019-08-27 00:40:43 -0700

    initial commit

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, flash
 from flask_bcrypt import Bcrypt
-from mysql_connection import connectToMySQL
+from mysqlconnection import connectToMySQL
 import re 
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
